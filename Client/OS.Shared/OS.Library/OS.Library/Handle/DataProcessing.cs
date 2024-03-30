134723b06ca702604f2ec62ae3b59096d3937394
ChauNgocMinh <chaungocminh202@gmail.com>
2024-03-31 01:24:53 +0700

    first commit

---
+++ Client/OS.Shared/OS.Library/OS.Library/Handle/DataProcessing.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OS.Library.Handle
+{
+    public class DataProcessing
+    {
+    }
+}
